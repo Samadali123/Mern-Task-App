ef888aaa283ae2a4a13bb615d5c3f4cd5876af7f
syed samad ali <samadali0125@gmail.com>
2025-04-23 15:40:47 +0530

    upload code

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const { createDbConnection } = require("./config/db");
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT
+const logger = require("morgan")
+const cors = require("cors")
+
+
+
+// create db connection
+createDbConnection();
+
+
+// reading json data and request body 
+
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
+
+// logging requests 
+app.use(logger("dev"))
+
+
+//aply cors
+app.use(cors());
+
+
+// stitch the routes to the server
+require("./routes/task.routes")(app);
+
+
+app.listen(PORT, ()=>{
+    console.log(` Server started on port ${PORT}`)
+})
+
+
+module.exports = app;
